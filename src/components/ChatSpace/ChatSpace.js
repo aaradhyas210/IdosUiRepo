8bd36ed7ab449bd60e1d7393e634f7a3a0ce9587
Aaradhya Sharma <aaradhyasharma@Aaradhyas-MacBook-Pro.local>
2023-05-10 13:15:55 +0530

    Initial Commit with UI

--- src/components/ChatSpace/ChatSpace.js
+++ src/components/ChatSpace/ChatSpace.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { IconButton, InputAdornment, TextField, styled } from "@mui/material";
-import SendIcon from "@mui/icons-material/Send";
-import AttachFileIcon from "@mui/icons-material/AttachFile";
-import CloseIcon from "@mui/icons-material/Close";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, CircularProgress, styled } from "@mui/material";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import DescriptionIcon from "@mui/icons-material/Description";
 
 const ChatSpace = () => {
+	const drop = useRef(null);
 	const [chatHistory, setChatHistory] = useState([]);
-	const [messageValue, setMessageValue] = useState("");
-	const [selectedImages, setSelectedImages] = useState([]);
-	const [selectedImageUrls, setSelectedImageUrls] = useState([]);
+	const [selectedFile, setSelectedFile] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
 		let chat = document.getElementById("chat");
@@ -17,12 +16,54 @@ const ChatSpace = () => {
 		}
 	}, [chatHistory]);
 
-	const getChatResponse = async (value) => {
-		fetch(`http://localhost:8080/api/question=${value}`)
+	useEffect(() => {
+		drop?.current?.addEventListener("dragover", HandleDragOver);
+		drop?.current?.addEventListener("drop", HandleDrop);
+		return () => {
+			// eslint-disable-next-line
+			drop?.current?.removeEventListener("dragover", HandleDragOver);
+			// eslint-disable-next-line
+			drop?.current?.removeEventListener("drop", HandleDrop);
+		};
+		// eslint-disable-next-line
+	}, []);
+
+	const HandleDragOver = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+	};
+
+	const HandleDrop = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		setSelectedFile(e.dataTransfer.files[0]);
+	};
+
+	const UploadFile = () => {
+		if (selectedFile) {
+			const chat = {
+				message: "",
+				sender: "client",
+				files: [selectedFile],
+			};
+			setChatHistory((curr) => [...curr, chat]);
+			getChatResponse();
+		}
+	};
+
+	const getChatResponse = async () => {
+		setIsLoading(true);
+		let formData = new FormData();
+		formData.append("file", selectedFile);
+		fetch(`http://localhost:8080/upload`, {
+			method: "POST",
+			body: formData,
+		})
 			.then((res) => {
 				if (res.ok) {
 					return res.json();
 				} else {
+					setIsLoading(false);
 					throw new Error("Something went wrong ...");
 				}
 			})
@@ -31,102 +72,86 @@ const ChatSpace = () => {
 					message: data,
 					sender: "bot",
 					files: [],
-					fileUrls: [],
 				};
 				setChatHistory((curr) => [...curr, chat]);
+				setSelectedFile(null);
+				setIsLoading(false);
 			})
-			.catch((error) => console.log(error));
+			.catch((error) => {
+				console.log(error);
+				setIsLoading(false);
+			});
 	};
 
-	const onMessageInputChange = (e) => {
-		setMessageValue(e.target.value);
-	};
-
-	const checkSendMessage = (e) => {
-		if (e.key === "Enter" && !e.shiftKey) {
-			sendMessage(e.target.value);
-		}
-	};
-
-	const sendMessage = (value) => {
-		if (value !== "") {
-			const chat = {
-				message: value,
-				sender: "client",
-				files: selectedImages,
-				fileUrls: selectedImageUrls,
-			};
-			setChatHistory([...chatHistory, chat]);
-			setMessageValue("");
-			setSelectedImages([]);
-			setSelectedImageUrls([]);
-			setTimeout(() => {
-				getChatResponse(value);
-			}, 1000);
-		}
-	};
-
-	const chooseFile = () => {
+	const BrowseFile = () => {
 		document.getElementById("fileSelector").click();
 	};
 
-	const onFileChange = (e) => {
-		const files = e.target.files;
-		if (files && files.length > 0) {
-			Object.values(files).forEach((element) => {
-				console.log(selectedImages.includes(element));
-				if (!selectedImages.includes(element)) {
-					setSelectedImages((curr) => [...curr, element]);
-					setSelectedImageUrls((curr) => [
-						...curr,
-						URL.createObjectURL(element),
-					]);
-				}
-			});
-		}
+	const OnFileChange = (e) => {
+		setSelectedFile(e.target.files[0]);
 	};
 
-	const removeFile = (index) => {
-		const newSelectedImages = [...selectedImages];
-		const newSelectedImageUrls = [...selectedImageUrls];
-		newSelectedImages.splice(index, 1);
-		newSelectedImageUrls.splice(index, 1);
-		setSelectedImages(newSelectedImages);
-		setSelectedImageUrls(newSelectedImageUrls);
+	const DownloadFile = (file, fileUrl) => {
+		const link = document.createElement("a");
+		link.href = fileUrl;
+		link.setAttribute("download", `${file.name}`);
+		document.body.appendChild(link);
+		link.click();
 	};
 
 	return (
 		<PageContainer>
 			<PageBackground />
-			<Overlay />
 			<Wrapper>
 				<ChatDisplaySection id="chat">
 					<ChatSection>
+						<ChatBubbleWrapper className={"BotChat"}>
+							<ChatBubbleSection className={"BotChat"}>
+								<IdentifierText>{"RFPBot"}</IdentifierText>
+								<ChatBubble className={"BotChat"}>
+									Hi I am RFPBot, your RFP Document generation companion. I have
+									been trained on data from the{" "}
+									<a
+										rel="noreferrer"
+										target="_blank"
+										style={{ color: "#3477eb" }}
+										href="https://www.acquisition.gov/browse/index/far">
+										Federal Acquisition Regulation
+									</a>{" "}
+									website to understand the context of the required document and
+									collate accurate details. <br /> <br />
+									All you need to do is share a Table of Contents in a excel
+									sheet and I will generate a detailed RFP Document for you.
+								</ChatBubble>
+							</ChatBubbleSection>
+						</ChatBubbleWrapper>
 						{chatHistory?.map((chat, index) => (
 							<>
-								<ChatBubbleWrapper
-									key={index}
-									className={
-										chat.sender === "client" ? "ClientChat" : "BotChat"
-									}>
-									<ChatBubbleSection
+								{chat.message !== "" && (
+									<ChatBubbleWrapper
+										key={index}
 										className={
 											chat.sender === "client" ? "ClientChat" : "BotChat"
 										}>
-										<IdentifierText>
-											{chat.sender === "client" ? "You" : "DermaBot"}
-										</IdentifierText>
-										<ChatBubble
+										<ChatBubbleSection
 											className={
 												chat.sender === "client" ? "ClientChat" : "BotChat"
 											}>
-											{chat.message}
-										</ChatBubble>
-									</ChatBubbleSection>
-								</ChatBubbleWrapper>
-								{chat.fileUrls?.length > 0 && (
+											<IdentifierText>
+												{chat.sender === "client" ? "You" : "DermaBot"}
+											</IdentifierText>
+											<ChatBubble
+												className={
+													chat.sender === "client" ? "ClientChat" : "BotChat"
+												}>
+												{chat.message}
+											</ChatBubble>
+										</ChatBubbleSection>
+									</ChatBubbleWrapper>
+								)}
+								{chat.files?.length > 0 && (
 									<>
-										{chat.fileUrls?.map((image, index) => (
+										{chat.files?.map((file, index) => (
 											<ChatBubbleWrapper
 												key={index}
 												className={
@@ -137,7 +162,7 @@ const ChatSpace = () => {
 														chat.sender === "client" ? "ClientChat" : "BotChat"
 													}>
 													<IdentifierText>
-														{chat.sender === "client" ? "You" : "DermaBot"}
+														{chat.sender === "client" ? "You" : "PDFBot"}
 													</IdentifierText>
 													<ChatBubble
 														className={
@@ -145,7 +170,23 @@ const ChatSpace = () => {
 																? "ClientChat"
 																: "BotChat"
 														}>
-														<ChatImage src={image} />
+														<FileShowContainer
+															onClick={() =>
+																DownloadFile(file, URL.createObjectURL(file))
+															}>
+															<DescriptionIcon
+																style={{
+																	fontSize: 40,
+																	color:
+																		chat.sender === "client"
+																			? "#1D6F42"
+																			: "#CC0000",
+																}}
+															/>
+															<UploadText className="small">
+																{file?.name}
+															</UploadText>
+														</FileShowContainer>
 													</ChatBubble>
 												</ChatBubbleSection>
 											</ChatBubbleWrapper>
@@ -154,67 +195,57 @@ const ChatSpace = () => {
 								)}
 							</>
 						))}
-
-						<ChatBubbleWrapper className={"BotChat"}>
-							<ChatBubbleSection className={"BotChat"}>
-								<IdentifierText>{"DermaBot"}</IdentifierText>
-								<ChatBubble className={"BotChat"}>
-									Lorem Ipsum is simply dummy text of the printing and
-									typesetting industry. Lorem Ipsum has been the industry's
-									standard dummy text ever since the 1500s, when an unknown
-									printer took a galley of type and scrambled it to make a type
-									specimen book. It has survived not only five centuries, but
-									also the leap into electronic typesetting, remaining
-									essentially unchanged. It was popularised in the 1960s with
-									the release of Letraset sheets containing Lorem Ipsum
-									passages, and more recently with desktop publishing software
-									like Aldus PageMaker including versions of Lorem Ipsum.
-								</ChatBubble>
-							</ChatBubbleSection>
-						</ChatBubbleWrapper>
 					</ChatSection>
 				</ChatDisplaySection>
 
 				<ChatInputWrapper>
-					<ChatInput
-						placeholder="Type your message here..."
-						onChange={onMessageInputChange}
-						onKeyUp={checkSendMessage}
-						value={messageValue}
-						multiline
-						InputProps={{
-							endAdornment: (
-								<InputAdornment position="end">
-									<IconButton onClick={chooseFile}>
-										<AttachFileIcon />
-										<HiddenInput
-											multiple
-											id="fileSelector"
-											accept="image/*"
-											type="file"
-											onChange={onFileChange}
-										/>
-									</IconButton>
-									<IconButton onClick={() => sendMessage(messageValue)}>
-										<SendIcon />
-									</IconButton>
-								</InputAdornment>
-							),
-						}}
-					/>
-					{selectedImageUrls?.length > 0 && (
-						<ImagePreviewContainer>
-							{selectedImageUrls.map((url, index) => (
-								<ImagePreviewWrapper key={index}>
-									<ImagePreview src={url} />
-									<RemoveButton onClick={() => removeFile(index)}>
-										<CloseIcon style={{ fontSize: "16px" }} />
-									</RemoveButton>
-								</ImagePreviewWrapper>
-							))}
-						</ImagePreviewContainer>
-					)}
+					<FileDropZone ref={drop}>
+						<BrowseContainer>
+							<CloudUploadIcon
+								style={{ fontSize: 60, color: "#2D2D2D", marginRight: "20px" }}
+							/>
+							<UploadText className="large">Drag file to upload</UploadText>
+							<UploadText className="small">Or</UploadText>
+							<BrowseFileButton onClick={BrowseFile}>
+								Browse Files
+							</BrowseFileButton>
+							<HiddenInput
+								id="fileSelector"
+								accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+								type="file"
+								onChange={OnFileChange}
+							/>
+						</BrowseContainer>
+						{selectedFile && (
+							<FilePreviewContainer>
+								<DescriptionIcon
+									style={{
+										fontSize: 40,
+										color: "#1D6F42",
+									}}
+								/>
+								<UploadText className="small">
+									{selectedFile?.name}
+									<br />{" "}
+									<span style={{ color: "rgb(0,0,0,0.5)", fontSize: "12px" }}>
+										{Math.round(selectedFile?.size / 100) / 10} KB
+									</span>
+								</UploadText>
+							</FilePreviewContainer>
+						)}
+					</FileDropZone>
 				</ChatInputWrapper>
+				{selectedFile && (
+					<UploadButtonContainer>
+						{isLoading ? (
+							<CircularProgress
+								style={{ color: "#2D2D2D", marginBottom: "20px" }}
+							/>
+						) : (
+							<UploadButton onClick={UploadFile}>Upload File</UploadButton>
+						)}
+					</UploadButtonContainer>
+				)}
 			</Wrapper>
 		</PageContainer>
 	);
@@ -236,23 +267,12 @@ const PageBackground = styled("div")({
 	left: 0,
 	width: "100%",
 	height: "100%",
-	backgroundColor: "#102310",
+	backgroundImage: "url(/PwC_Geom_02.png)",
+	backgroundSize: "cover",
+	backgroundRepeat: "no-repeat",
+	backgroundPosition: "center",
+	filter: "contrast(0.75)",
 	zIndex: 0,
-});
-
-const Overlay = styled("div")({
-	position: "absolute",
-	top: 0,
-	left: 0,
-	width: "100%",
-	height: "100%",
-	backgroundImage: "url(/stethoscope.png)",
-	backgroundSize: "repeat",
-	backgroundRepeat: "space",
-	backgroundPosition: "left top",
-	mixBlendMode: "overlay",
-	filter: "contrast(0.35)",
-	zIndex: 1,
 });
 
 const Wrapper = styled("div")({
@@ -302,7 +322,7 @@ const ChatBubbleWrapper = styled("div")({
 });
 
 const ChatBubbleSection = styled("div")({
-	width: "60%",
+	width: "50%",
 	display: "flex",
 	marginBottom: "10px",
 	flexDirection: "column",
@@ -318,19 +338,19 @@ const ChatBubbleSection = styled("div")({
 const IdentifierText = styled("div")({
 	fontSize: "12px",
 	fontWeight: 500,
-	color: "rgba(255, 255, 255, 0.4)",
+	color: "#2D2D2D",
 	marginBottom: "7px",
 });
 
 const ChatBubble = styled("div")({
 	padding: "15px",
-	fontSize: "14px",
+	fontSize: "15PX",
 	zIndex: 1000,
 	textAlign: "left",
 	whiteSpace: "pre-wrap",
 	"&.BotChat": {
-		backgroundColor: "rgb(32, 70, 32, 0.75)",
-		color: "rgb(255, 255, 255, 0.8)",
+		backgroundColor: "#2D2D2D",
+		color: "rgb(255, 255, 255, 0.75)",
 		borderRadius: "5px 20px 20px 20px",
 	},
 	"&.ClientChat": {
@@ -340,81 +360,114 @@ const ChatBubble = styled("div")({
 	},
 });
 
-const ChatImage = styled("img")({
-	width: "400px",
-	objectFit: "cover",
+const FileShowContainer = styled("div")({
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
+	padding: "5px 10px",
 	borderRadius: "10px",
+	backgroundColor: "rgba(255, 255, 255)",
+	boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.75)",
+	cursor: "pointer",
+	color: "#2D2D2D",
 });
 
 const ChatInputWrapper = styled("div")({
-	width: "-webkit-fill-available",
+	width: "60%",
+	minHeight: "200px",
 	display: "flex",
 	flexDirection: "column",
 	alignItems: "flex-st",
 	zIndex: 2,
-	padding: "0px 10px",
-	margin: "20px 20px",
-	backgroundColor: "rgba(255, 255, 255)",
+	padding: "10px 10px",
+	margin: "50px 20px",
+	backgroundColor: "rgba(255, 255, 255, 0.8)",
 	borderRadius: "10px",
 	borderColor: "#193718",
 });
 
-const ChatInput = styled(TextField)({
-	resize: "none",
-	width: "100%",
-	borderRadius: "10px",
-	fontFamily: "inherit",
-	border: "none",
-	backgroundColor: "rgba(255, 255, 255, 0.8)",
-	"& .MuiOutlinedInput-root": {
-		"& fieldset": {
-			borderColor: "#FFFFFF",
-		},
-		"&:hover fieldset": {
-			borderColor: "#FFFFFF",
-		},
+const FileDropZone = styled("div")({
+	display: "flex",
+	flexDirection: "column",
+	alignItems: "center",
+	justifyContent: "center",
+	height: "100%",
+	border: "3px dashed #2D2D2D",
+	borderRadius: "20px",
+});
+
+const BrowseContainer = styled("div")({
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
+	color: "#2D2D2D",
+	fontFamily: "Helvetica Neue",
+});
+
+const UploadText = styled("div")({
+	fontWeight: 350,
+	textAlign: "left",
+	marginLeft: "10px",
+	"&.large": {
+		fontSize: "20px",
 	},
-	"& .MuiOutlinedInput-root.Mui-focused": {
-		"& > fieldset": {
-			borderColor: "#FFFFFF",
-		},
+	"&.small": {
+		fontSize: "15px",
+	},
+});
+
+const BrowseFileButton = styled(Button)({
+	background: "#2D2D2D",
+	color: "#FFFFFF",
+	fontWeight: 400,
+	fontSize: "15px",
+	padding: "5px 20px",
+	marginLeft: "20px",
+	borderRadius: "30px",
+	"&:hover": {
+		background: "#2D2D2D",
+		opacity: 0.8,
+	},
+});
+
+const FilePreviewContainer = styled("div")({
+	display: "flex",
+	flexDirection: "row",
+	justifyContent: "center",
+	alignItems: "center",
+	marginTop: "10px",
+	padding: "10px",
+	backgroundColor: "rgba(0, 0, 0, 0.2)",
+	boxShadow: "inset 0px 0px 10px 0px rgba(0, 0, 0, 0.2)",
+	borderRadius: "10px",
+});
+
+const UploadButtonContainer = styled("div")({
+	display: "flex",
+	flexDirection: "row",
+	justifyContent: "center",
+	alignItems: "center",
+	width: "100%",
+	marginBottom: "20px",
+	marginTop: "-20px",
+	height: "50px",
+});
+
+const UploadButton = styled(Button)({
+	background: "#2D2D2D",
+	color: "#FFFFFF",
+	fontSize: "20px",
+	padding: "10px 30px",
+	borderRadius: "30px",
+	"&:hover": {
+		background: "#EB8C00",
+		color: "#2D2D2D",
+		opacity: 0.8,
 	},
 });
 
 const HiddenInput = styled("input")({
 	display: "none",
-});
-
-const ImagePreviewContainer = styled("div")({
-	display: "flex",
-	flexDirection: "row",
-	justifyContent: "flex-start",
-	alignItems: "center",
-	flexWrap: "wrap",
-	width: "100%",
-	marginBottom: "10px",
-});
-
-const ImagePreview = styled("img")({
-	width: "100px",
-	height: "100px",
-	objectFit: "cover",
-	borderRadius: "10px",
-	position: "relative",
-	marginLeft: "15px",
-});
-
-const ImagePreviewWrapper = styled("div")({
-	position: "relative",
-	marginTop: "10px",
-});
-
-const RemoveButton = styled(IconButton)({
-	position: "absolute",
-	top: -5,
-	right: -5,
-	color: "#000000",
-	backgroundColor: "rgba(0,0,0,0.3)",
 });
 
 export default ChatSpace;
